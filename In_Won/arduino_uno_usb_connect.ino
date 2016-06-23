47a3866fb58c0625fe489df88eec6ee43199901d
deinstall <ren.rausch.13@lehre.mosbach.dhbw.de>
2016-06-23 10:34:16 +0200

    Add files via upload

---
+++ In_Won/arduino_uno_usb_connect.ino
@@ -0,0 +1,27 @@
+int number = 0;
+void setup(){
+     Serial.begin(9600);
+       pinMode(13, OUTPUT);
+   
+}
+
+void loop(){
+  int incomingBytes;
+   if (Serial.available() > 0)
+   {
+       //Serial.print("character recieved: ");
+       //Serial.println(number, DEC);
+       incomingBytes = Serial.read();
+       
+       if(incomingBytes == '1'){
+        Serial.print("sending...");
+       digitalWrite(13, HIGH);
+      delay(1000);
+          }
+   
+      else{
+        digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+        }
+   }
+}
+
